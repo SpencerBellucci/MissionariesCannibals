2af0704b73621fedb83089a6db8c845394b577e3
SpencerBellucci <spencer.bellucci@mymail.champlain.edu>
2021-02-10 17:50:35 -0500

    finished assignment2

--- missionaries.go
+++ missionaries.go
@@ -1,5 +1,8 @@
 // Missionaries and Cannibals for CSI 380
 // This program solves the classic Missionaries and Cannibals problem
+// Spencer Bellucci
+// CSI 380
+// 2/10/21
 
 package main
 
@@ -17,19 +20,188 @@ type position struct {
 // Is this a legal position? In particular, does it have
 // more cannibals than missionaries on either bank? Because that is illegal.
 func valid(pos position) bool {
-	// YOUR CODE GOES HERE
+	westSide := false
+	eastSide := false
+
+	// make a negative false
+	if pos.westMissionaries < 0 || pos.eastMissionaries < 0 || pos.westCannibals < 0 || pos.eastCannibals < 0 {
+		return false
+	}
+
+	// make anything more than 3 false
+	if pos.westMissionaries > 3 || pos.eastMissionaries > 3 || pos.westCannibals > 3 || pos.eastCannibals > 3 {
+		return false
+	}
+
+	// check if west side is valid
+	if pos.westMissionaries >= pos.westCannibals || pos.westMissionaries == 0 {
+		westSide = true
+	} else {
+		westSide = false
+	}
+
+	//check if east side is valid
+	if pos.eastMissionaries >= pos.eastCannibals || pos.eastMissionaries == 0 {
+		eastSide = true
+	} else {
+		eastSide = false
+	}
+
+	//if both sides are valid return true
+	if westSide && eastSide == true {
+		return true
+	} else {
+		return false
+	}
 }
 
 // What are all of the next positions we can go to legally from the current position
 // Returns nil if there are no valid positions
 func (pos position) successors() []position {
-	// YOUR CODE GOES HERE
+	var allPositions []position
+
+	// if boat is on west bank
+	if pos.boatOnWestBank == true {
+		// check both sides with - 1 missionary, then - 2
+		for i := 1; i < 3; i++ {
+			pos.westMissionaries = pos.westMissionaries - i
+			pos.eastMissionaries = pos.eastMissionaries + i
+			pos.boatOnWestBank = false
+			if valid(pos) {
+				newPos := pos
+				allPositions = append(allPositions, newPos)
+			}
+			// set back to normal
+			pos.eastMissionaries = pos.eastMissionaries - i
+			pos.westMissionaries = pos.westMissionaries + i
+			pos.boatOnWestBank = true
+		}
+
+		// do the same thing with cannibals
+		for i := 1; i < 3; i++ {
+			pos.westCannibals = pos.westCannibals - i
+			pos.eastCannibals = pos.eastCannibals + i
+			pos.boatOnWestBank = false
+			if valid(pos) {
+				newPos := pos
+				allPositions = append(allPositions, newPos)
+			}
+			pos.eastCannibals = pos.eastCannibals - i
+			pos.westCannibals = pos.westCannibals + i
+			pos.boatOnWestBank = true
+		}
+
+		// check with one missionary and one cannibal
+		pos.westCannibals = pos.westCannibals - 1
+		pos.eastCannibals = pos.eastCannibals + 1
+		pos.westMissionaries = pos.westMissionaries - 1
+		pos.eastMissionaries = pos.eastMissionaries + 1
+		pos.boatOnWestBank = false
+		if valid(pos) {
+			newPos := pos
+			allPositions = append(allPositions, newPos)
+		}
+		pos.eastCannibals = pos.eastCannibals - 1
+		pos.westCannibals = pos.westCannibals + 1
+		pos.westMissionaries = pos.westMissionaries + 1
+		pos.eastMissionaries = pos.eastMissionaries - 1
+		pos.boatOnWestBank = true
+
+	} else if pos.boatOnWestBank == false {
+		// check both sides with - 1 missionary, then - 2
+		for i := 1; i < 3; i++ {
+			pos.eastMissionaries = pos.eastMissionaries - i
+			pos.westMissionaries = pos.westMissionaries + i
+			pos.boatOnWestBank = true
+			if valid(pos) {
+				newPos := pos
+				allPositions = append(allPositions, newPos)
+			}
+			// set back to normal
+			pos.westMissionaries = pos.westMissionaries - i
+			pos.eastMissionaries = pos.eastMissionaries + i
+			pos.boatOnWestBank = false
+		}
+
+		// do the same thing with cannibals
+		for i := 1; i < 3; i++ {
+			pos.eastCannibals = pos.eastCannibals - i
+			pos.westCannibals = pos.westCannibals + i
+			pos.boatOnWestBank = true
+			if valid(pos) {
+				newPos := pos
+				allPositions = append(allPositions, newPos)
+			}
+			pos.westCannibals = pos.westCannibals - i
+			pos.eastCannibals = pos.eastCannibals + i
+			pos.boatOnWestBank = false
+		}
+
+		// check with one missionary and one cannibal
+		pos.eastCannibals = pos.eastCannibals - 1
+		pos.westCannibals = pos.westCannibals + 1
+		pos.eastMissionaries = pos.eastMissionaries - 1
+		pos.westMissionaries = pos.westMissionaries + 1
+		pos.boatOnWestBank = true
+		if valid(pos) {
+			newPos := pos
+			allPositions = append(allPositions, newPos)
+		}
+		pos.westCannibals = pos.westCannibals - 1
+		pos.eastCannibals = pos.eastCannibals + 1
+		pos.eastMissionaries = pos.eastMissionaries + 1
+		pos.westMissionaries = pos.westMissionaries - 1
+		pos.boatOnWestBank = false
+
+	}
+
+	// return array if there were any valid positions
+	if len(allPositions) != 0 {
+		return allPositions
+	} else {
+		return nil
+	}
+
 }
 
 // A recursive depth-first search that goes through to find the goal and returns the path to get there
 // Returns nil if no solution found
 func dfs(start position, goal position, solution []position, visited map[position]bool) []position {
-	// YOUR CODE GOES HERE
+	// mark solution as visited
+	visited[start] = true
+	// add current move to solution
+	newSolution := append(solution, start)
+
+	// if this state != goal
+	if start != goal {
+		// find all valid moves from this state
+		var branches = start.successors()
+
+		// if no valid moves
+		if branches == nil {
+			// go back to previous move
+			newSolution = solution[:len(solution)-1]
+			return dfs(start, goal, newSolution, visited)
+		}
+
+		// check for a move that has not been visited
+		for i := 0; i < len(branches); i++ {
+			_, ok := visited[branches[i]]
+			if !ok {
+				// call function with new move
+				return dfs(branches[i], goal, newSolution, visited)
+			}
+		}
+
+		// go back to previous move
+		newSolution = solution[:len(solution)-1]
+		return dfs(solution[len(solution)-1], goal, newSolution, visited)
+
+	} else {
+		// return solution
+		newSolution = solution[1 : len(solution)+1]
+		return newSolution
+	}
 }
 
 func main() {
